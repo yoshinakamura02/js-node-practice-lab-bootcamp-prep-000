901b02e04cf38c4d864e2788a759df2e4a459c62
Yoshinori Nakamura <ynakamura@rockefellergroup.com>
2018-09-10 12:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Janet"
 var height = "74"
 
-console.log("name")
+console.log("Name:")
 console.log(name)
 
-console.log("Height")
-console.log(height+1)
+console.log("Height:")
+console.log(parseInt(height) + 1)
